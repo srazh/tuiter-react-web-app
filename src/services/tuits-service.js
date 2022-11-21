456833f89146b95a823aa4e4e2458b241d96d827
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-11-20 23:18:03 -0500

    edits for netlify deployment

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -26,7 +26,7 @@ export const deleteTuit = async (tid) => {
 
 
 export const updateTuit = async (tuit) => {
-    const response = await axios
+    await axios
         .put(`${TUITS_API}/${tuit._id}`, tuit);
     return tuit;
 }
--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import tuits from './tuits.json';
+//import tuits from './tuits.json';
 import {createTuitThunk, deleteTuitThunk, findTuitsThunk, updateTuitThunk}
     from "../../services/tuits-thunks";
 
